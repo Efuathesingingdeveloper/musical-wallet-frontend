40ee6336a1e9ac8aa490954b7010faa0164c1943
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2021-03-16 11:19:21 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import  React from 'react';
+
+
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/accounts/1')
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  }
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+    );
+    
+  }
+ 
+}
+
+export default App;
